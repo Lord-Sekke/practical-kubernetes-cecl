c6b1cd6afb81c9043123eebe9c06d89ec70af9c5
CourseMaterial <55927635+CourseMaterial@users.noreply.github.com>
2022-03-06 20:37:06 +0500

    Add files via upload

--- commands.sh
+++ commands.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Installing jq
+sudo yum install jq -y
+
 # Installing Kops
 wget -O kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x ./kops
@@ -29,6 +32,41 @@ kops update cluster $NAME --yes
 # Rolling cluster
 kops rolling-update cluster --cloudonly --yes
 
+# Retreiving Zones
+aws ec2 describe-instances \
+    | jq -r \
+    ".Reservations[].Instances[] \
+    | select(.SecurityGroups[]\
+    .GroupName==\"nodes.$NAME\")\
+    .Placement.AvailabilityZone" \
+    | tee zones
+
+AZ_1=$(cat zones | head -n 1)
+
+AZ_2=$(cat zones | tail -n 1)
+
+# Creating EBS Volums
+VOLUME_ID_1=$(aws ec2 create-volume \
+    --availability-zone $AZ_1 \
+    --size 10 \
+    --volume-type gp2 \
+    --tag-specifications "ResourceType=volume,Tags=[{Key=KubernetesCluster,Value=$NAME}]" \
+    | jq -r '.VolumeId')
+
+VOLUME_ID_2=$(aws ec2 create-volume \
+    --availability-zone $AZ_1 \
+    --size 10 \
+    --volume-type gp2 \
+    --tag-specifications "ResourceType=volume,Tags=[{Key=KubernetesCluster,Value=$NAME}]" \
+    | jq -r '.VolumeId')
+
+VOLUME_ID_3=$(aws ec2 create-volume \
+    --availability-zone $AZ_2 \
+    --size 10 \
+    --volume-type gp2 \
+    --tag-specifications "ResourceType=volume,Tags=[{Key=KubernetesCluster,Value=$NAME}]" \
+    | jq -r '.VolumeId')
+
 # Echo command
 echo "The script will be put on pause for the next 5 minutes so the Kubernetes master and worker nodes can be initialized properly"
 
@@ -44,6 +82,14 @@ cd ..
 # Installing Ingress
 kubectl create \
     -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/ingress-nginx/v1.6.0.yaml
+
+# Retrieving ELB DNS
+CLUSTER_DNS=$(aws elb \
+    describe-load-balancers | jq -r \
+    ".LoadBalancerDescriptions[] \
+    | select(.DNSName \
+    | contains (\"api-devops23\") \
+    | not).DNSName")
 
 # Creating persistent volumes
 cat pv/pv.yml \
